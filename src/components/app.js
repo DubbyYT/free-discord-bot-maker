fe0ccca998b91295fe607efbbdc0d447ec71d100
Phantom <CloudRex@users.noreply.github.com>
2018-06-25 16:47:23 -0400

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SelectProject from "./select-project";
+import BehaviourEditor from "./behaviour-editor/behaviour-editor";
+import "../styles/global.css";
+import CreateProject from "./create-project";
+
+export default class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={SelectProject} />
+                    <Route path="/behaviours" component={BehaviourEditor} />
+                    <Route path="/create" component={CreateProject} />
+                </div>
+            </Router>
+        );
+    }
+}
---
+++ src/components/select-project.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../styles/select-project.css";
+
+const DiscordPassport = require("passport-discord");
+
+export default class SelectProject extends Component {
+    constructor(props) {
+        super(props);
+
+        this.checkAuth();
+    }
+
+    render() {
+        return (
+            <div className="select-project">
+                <div className="box">
+                    <Link className="create" to="/create">Create Project</Link>
+                    <div className="list">
+                        <div className="item selected">Moderation Bot</div>
+                        <div className="item">My Chat Bot</div>
+                        <div className="item">Fun Bot</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
